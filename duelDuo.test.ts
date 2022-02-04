3269bd182aaa61f0d1d1b74bca76116a0848c955
Agriscapes <62765843+Agriscapes@users.noreply.github.com>
2022-02-04 17:45:22 -0600

    Added corrections for Rollbar, Automated Tests, and Unit Tests

---
+++ duelDuo.test.ts
@@ -0,0 +1,46 @@
+import { Builder, Capabilities, By } from "selenium-webdriver"
+
+require('chromedriver')
+
+const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
+
+beforeEach(async () => {
+    driver.get('http://localhost:3000/')
+})
+
+afterAll(async () => {
+    driver.quit()
+})
+
+test('Title shows up when page loads', async () => {
+    const title = await driver.findElement(By.id('title'))
+    const displayed = await title.isDisplayed()
+    expect(displayed).toBe(true)
+
+    await driver.sleep(2000)
+})
+
+test('Draw button displays choices', async () => {
+    let button = await driver.findElement(By.id('draw'))
+    let choices = await driver.findElement(By.id('choices'))
+
+    await button.click()
+
+    await driver.sleep(2000)
+})
+
+test('Add to Duo button adds robot to player', async () => {
+    let button = await driver.findElement(By.id('draw'))
+    let choices = await driver.findElement(By.id('choices'))
+
+    await button.click()
+
+    await driver.sleep(2000)
+
+    let duoBtn = await driver.findElement(By.className('bot-btn'))
+
+    await duoBtn.click()
+
+    await driver.sleep(5000)
+
+})
